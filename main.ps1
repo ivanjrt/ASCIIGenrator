3ec7035f3679e94d00f711cf4943555b0f003c67
ivanjrt <44326428+ivanjrt@users.noreply.github.com>
2020-12-16 22:59:34 -0600

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,5 @@
-$Font = "com_sen_" #for a full listings https://artii.herokuapp.com/fonts_list
+$Font         = "com_sen_" #for a full listings https://artii.herokuapp.com/fonts_list
 $TextToEncode = [uri]::EscapeDataString("POWER")
-$url = "http://artii.herokuapp.com/make?text=$TextToEncode&font=$Font"
-$ouput = Invoke-Restmethod -Uri $url -DisableKeepAlive -ErrorAction Stop
-Write-Host -ForegroundColor red $ouput -BackgroundColor Black
+$url          = "http://artii.herokuapp.com/make?text=$TextToEncode&font=$Font"
+$ouput        = Invoke-Restmethod -Uri $url -DisableKeepAlive -ErrorAction Stop
+Write-Host    -ForegroundColor red $ouput -BackgroundColor Black
